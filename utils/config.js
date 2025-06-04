c2af720a0a641c0676274c61cb1a19de5f62d788
Basper <lasperbasper@gmail.com>
2025-06-04 06:14:20 +0200

    eaiser way to create config files in modules

---
+++ utils/config.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+function loadConfig(filePath, defaultData) {
+  if (!fs.existsSync(filePath)) {
+    fs.mkdirSync(path.dirname(filePath), { recursive: true });
+    saveConfig(filePath, defaultData);
+    return defaultData;
+  }
+  return require(filePath);
+}
+
+function saveConfig(filePath, data) {
+  fs.writeFileSync(filePath, `module.exports = ${JSON.stringify(data, null, 2)};\n`);
+}
+
+module.exports = { loadConfig, saveConfig };
